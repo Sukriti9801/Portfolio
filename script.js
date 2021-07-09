2d3c30e60708c2875ce886226f12aa2937f75be5
Sukriti9801 <72074761+Sukriti9801@users.noreply.github.com>
2021-07-10 00:43:06 +0530

    Adding initial file for portfolio website

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  //console.log = ("loaded")
+  var typed = new Typed('#typed', {
+    strings: ["Welcome to my profile"],
+    backspeed: 15,
+    smartbackspace : true,
+    backDelay: 1200,
+    startDelay: 1000,
+    typespeed: 25,
+    loop: true,
+  });
+};
